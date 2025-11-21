d463a951c2a12fdcc1304a3b7c2981c2f3ce7d86
khshlgrvr <khshlgrvr@gmail.com>
2025-11-21 18:38:57 +0530

    config for hosting

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'R3F-Texture-Config-Start',
   plugins: [react()],
   server: {
     host: true
