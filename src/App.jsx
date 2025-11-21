79edb748f42110141b8bd31069b44a93db2a8bd5
khshlgrvr <khshlgrvr@gmail.com>
2025-11-21 18:16:25 +0530

    post processing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import { useControls, button } from 'leva'
+import { EffectComposer, Bloom, DepthOfField, Noise, Vignette } from '@react-three/postprocessing'
 
 function App() {
   useControls({
@@ -26,7 +27,12 @@ function App() {
     <>
       <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
-      <Experience />
+      <EffectComposer>
+        <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} />
+        <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
+        <Noise opacity={0.02} />
+        <Vignette eskil={false} offset={0.1} darkness={1.1} />
+      </EffectComposer>
     </Canvas>
     </>
     
